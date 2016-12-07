8671ecdbe29091b07b8550bd1ff85c5dcaa77610
ybbarng <ybbarng@gmail.com>
2016-12-07 20:02:12 +0900

    svg에 node들의 위치정보를 이용해서 노란 원을 그림

--- src/client.js
+++ src/client.js
@@ -7,6 +7,41 @@ $(function() {
   var parser = new dataParser.DataParser(onComplete);
   var nodes = [];
 
+  var svg_width = 600;
+  var svg_height = 600;
+
+  var x = d3.scale.linear().domain([-10, 130]).range([0, svg_width]);
+  var y = d3.scale.linear().domain([-10, 130]).range([0, svg_height]);
+
+  var chart = d3.select('#visualizer')
+    .append('svg').attr({width: svg_width, height: svg_height})
+    .append('g');
+
+  var elementsGroup = chart.append('g')
+    .attr('class', 'elements');
+
+  function refresh() {
+    var nodeViews = elementsGroup.selectAll('circle')
+      .data(nodes);
+
+    nodeViews
+      .transition('update')
+      .duration(500)
+      .delay(function(d, i) {return 2 * i;})
+      .attr('transform', function(d) {
+        return 'translate(' + x(d.x) + ', ' + y(d.y) + ')';
+      })
+      .style('fill', 'yellow');
+
+    var newNodeViews = nodeViews.enter()
+      .append('circle')
+      .attr('transform', function(d) {
+        return 'translate(' + x(d.x) + ', ' + y(d.y) + ')';
+      })
+      .attr('r', 14)
+      .style('fill', 'yellow');
+  }
+
   function onComplete(csc, log) {
     initiateSimulation(csc);
   }
@@ -19,5 +54,6 @@ $(function() {
       var location = mote.interface_config[0];
       nodes.push(new node.Node(moteId, location.x, location.y));
     }
+    refresh();
   }
 });
