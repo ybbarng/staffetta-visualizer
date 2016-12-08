7068e59d808927dc7151a27a2d51a4d2b8066f45
ybbarng <ybbarng@gmail.com>
2016-12-08 12:16:12 +0900

    flow의 길이가 최소 길이보다 짧은 경우 최소 길이로 맞춤

--- src/flow.js
+++ src/flow.js
@@ -13,15 +13,20 @@ exports.Flow.prototype = {
 
     var fromRadius = 4;
     var toRadius = 5;
-    var x1 = fromNode.x * 1;
-    var y1 = fromNode.y * 1;
-    var x2 = toNode.x * 1;
-    var y2 = toNode.y * 1;
-    var theta = Math.atan2(y2 - y1, x2 - x1);
-    this.x1 = x1 + fromRadius * Math.cos(theta);
-    this.y1 = y1 + fromRadius * Math.sin(theta);
-    this.x2 = x2 - toRadius * Math.cos(theta);
-    this.y2 = y2 - toRadius * Math.sin(theta);
+    this.x1 = fromNode.x * 1;
+    this.y1 = fromNode.y * 1;
+    this.x2 = toNode.x * 1;
+    this.y2 = toNode.y * 1;
+    var dy = this.y2 - this.y1;
+    var dx = this.x2 - this.x1;
+    if (dx * dx + dy * dy < Math.pow(fromRadius + toRadius, 2)) {
+      return;
+    }
+    var theta = Math.atan2(dy, dx);
+    this.x1 = this.x1 + fromRadius * Math.cos(theta);
+    this.y1 = this.y1 + fromRadius * Math.sin(theta);
+    this.x2 = this.x2 - toRadius * Math.cos(theta);
+    this.y2 = this.y2 - toRadius * Math.sin(theta);
   },
   toString: function() {
     return 'Flow: ' + this.fromNodeId + ' -> ' + this.toNodeId;
