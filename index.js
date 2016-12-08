4bc7fd4f60d924d9f8013c5f331dac74586c2cae
ybbarng <ybbarng@gmail.com>
2016-12-08 14:44:44 +0900

    data 파일 목록을 서버쪽에서 전달할 수 있도록 api 구현

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-var ecstatic = require('ecstatic');
-var server = require('http').createServer(
-  ecstatic({root: __dirname + '/app', handleError: false})
-);
+var http = require('http');
+var ecstatic = require('ecstatic')({root: __dirname + '/app', handleError: false});
+var router = require('routes')();
+var fs = require('fs');
+
+dataFolder = './app/data/';
+var dataList = [];
+fs.readdir(dataFolder, function(error, files) {
+  files.forEach(function(file) {
+    var dataName = file.split('.')[0];
+    if (dataList.indexOf(dataName) === -1) {
+      dataList.push(dataName);
+    }
+  });
+});
+
+router.addRoute('/datalist.json', function(req, res, params) {
+  res.end(JSON.stringify(dataList));
+});
+
+var server = http.createServer(function(req, res) {
+  var m = router.match(req.url);
+  if (m) {
+    m.fn(req, res, m.params);
+  } else {
+    ecstatic(req, res);
+  }
+});
 
 var port = 12025;
 server.listen(port, function() {
