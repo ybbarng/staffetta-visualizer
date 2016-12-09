7b8c949641140db6e45fd82478f803b038454ae8
ybbarng <ybbarng@gmail.com>
2016-12-09 11:37:04 +0900

    sink는 sink라고 적도록 함

--- src/node.js
+++ src/node.js
@@ -10,10 +10,14 @@ exports.Node = function(nodeId, x, y) {
 
 exports.Node.prototype = {
   setNodeId: function(nodeId) {
+    nodeId -= 1;
     var first = nodeId / 26;
     var second = nodeId % 26;
     this.nodeId = (String.fromCharCode(97 + first) + String.fromCharCode(97 + second)).toUpperCase();
-    this.isSink = nodeId === "1";
+    this.isSink = nodeId === 0;
+    if (this.isSink) {
+      this.nodeId = 'sink';
+    }
   },
   onAck: function(timestamp, sender_id, frequency) {
     if (this.isSink) {
